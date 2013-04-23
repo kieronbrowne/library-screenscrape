506603c12be4cf11721aa796f8a8f1c04a78b584
Kieron Browne <kieron.browne@totalobjects.co.uk>
2013-04-23 17:49:48 +0100

    almost working for new site

--- run.php
+++ run.php
@@ -4,7 +4,8 @@ if (!isset($argv[1]) or !isset($argv[2])) {
 	exit;
 }
 
-$loansURL = "https://www.londonlibraries.gov.uk/Richmond/01_YourAccount/01_002_YourLoans.aspx";
+$loansURL = "https://richmond.spydus.co.uk/cgi-bin/spydus.exe/MSGTRN/OPAC/HOME";
+$root = substr($loansURL, 0, strpos($loansURL, '/', 15));
 $myusername = $argv[1];
 $mypassword = $argv[2];
 $postVars = array();
@@ -22,7 +23,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 $data=curl_exec($ch);
 
 preg_match('/<form.*action="([^"]*)".*>/', $data, $matches);
-$logonAction = substr($loansURL, 0, strrpos($loansURL, '/')+1).$matches[1];
+$logonAction = $root.$matches[1];
 
 preg_match_all('/<input[^>]*>/', $data, $matches);
 foreach ($matches[0] as $match) {
@@ -31,29 +32,44 @@ foreach ($matches[0] as $match) {
 	$name = $names ? $names[1] : '';
 	$value = $values ? $values[1] : '';
 
-	if (strstr($name, '__') or strstr($name, 'PlaceCent')) {
-		if (strstr($name, 'username')) {
+	if (strstr($name, 'BRW') or strstr($name, 'ISGLB')) {
+		if (strstr($name, 'BRWLID')) {
 			$value = $myusername;
-		} else if (strstr($name, 'password')) {
+		} else if (strstr($name, 'BRWLPWD')) {
 			$value = $mypassword;
+		} else if (strstr($name, 'ISGLB')) {
+			$value = "1";
 		}
-		$postVars[$name] = $value;
+		$postVars[] = $name.'='.$value;
 	}
 }
-
 curl_setOpt($ch, CURLOPT_POST, TRUE);
-curl_setOpt($ch, CURLOPT_POSTFIELDS, $postVars);
+curl_setOpt($ch, CURLOPT_POSTFIELDS, implode($postVars, '&'));
 curl_setOpt($ch, CURLOPT_URL, $logonAction);
 
 $data = curl_exec($ch);
+$matches = array();
+preg_match('/url=(.*)"/', $data, $matches);
+
+curl_setOpt($ch, CURLOPT_URL, $matches[1]);
+curl_setOpt($ch, CURLOPT_POST, FALSE);
+$data = curl_exec($ch);
 
 $matches = array();
-preg_match_all('/TitleListResultsCenterStyle4Reduced.*<h2><a[^>]*>([^<]*)<.*<h2>By ([^<]*)<\/h2>.*<strong>Due Date:<\/strong>(\d{1,2}\/\d{1,2}\/\d{2})/Us', $data, $matches);
+preg_match('/<a href="([^<]*)">Current loans<\/a>/', $data, $matches);
+
+$query = explode('?', $matches[1]);
+curl_setOpt($ch, CURLOPT_URL, $root.str_replace("&amp;", "&", $matches[1]));
+$data=curl_exec($ch);
+
+//echo $data;
+
+$matches = array();
+preg_match_all('/<tr valign="top">.*<td.*>.*<\/td><td.*><a .*>(.*)<\/a>.*<\/td><td .*>(.*)<br \/><\/td>.*<\/tr>/s', $data, $matches);
 
 for ($i = 0; $i < count($matches[0]); $i++) {
 	$book = trim($matches[1][$i]);
-	$author = trim($matches[2][$i]);
-	$dueDate = date_create_from_format('d/m/y', $matches[3][$i]);
+	$dueDate = date_create_from_format('d M Y', $matches[2][$i]);
 	$daysRemaining = -$dueDate->diff(new DateTime())->format('%r%a');
 	printf("%s (%s) due back in %s days\n", $book, $author, $daysRemaining);
 }
